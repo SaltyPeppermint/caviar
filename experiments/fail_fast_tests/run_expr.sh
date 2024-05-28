2163f00a80985afc8272bc01f21c6711585a8d2e
Nicole Heinimann <heinimann@tu-berlin.de>
2024-05-28 10:15:11 +0200

    fixed experiment scripts

--- experiments/fail_fast_tests/run_expr.sh
+++ experiments/fail_fast_tests/run_expr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-../../target/release/egg_halides_trs prove_exprs_fast_passes ../../data/prefix/expressions_egg.csv 10000000 10000000 3 0.1
-../../target/release/egg_halides_trs prove_exprs_fast ../../data/prefix/expressions_egg.csv 10000000 10000000 3
+./target/release/caviar prove_exprs_fast_passes ./data/prefix/expressions_egg.csv 10000000 10000000 3 0.1
+./target/release/caviar prove_exprs_fast ./data/prefix/expressions_egg.csv 10000000 10000000 3
